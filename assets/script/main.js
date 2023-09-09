cca87735e0a336ff857a8cb49b3f6c4f81e0c86c
Kingsley <kingsleyakwa@gmail.com>
2023-09-09 12:01:25 +0100

    hng-task-1

---
+++ assets/script/main.js
@@ -0,0 +1,18 @@
+const currentDate = new Date();
+const weekDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+const currentTime = currentDate.getTime();
+const currentDay = weekDay[currentDate.getDay()];
+
+const time = document.querySelector("#currentUTCTime");
+const dayOfTheWeek = document.querySelector("#currentDayOfTheWeek");
+time.innerHTML = `${currentTime} ms`;
+dayOfTheWeek.innerHTML = currentDay;
